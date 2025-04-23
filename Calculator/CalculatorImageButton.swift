f9b169bffe54ab1d0b571aaf4ce75fe7358b50dc
Meghana <meghana.trivedi@visongroupretail.com>
2025-04-23 14:24:23 +0530

    Done the calculattore app in swift UI

---
+++ Calculator/CalculatorImageButton.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorImageButton.swift
+//  Calculator
+//
+//  Created by Meggi on 23/04/25.
+//
+
+import SwiftUI
+
+struct CalculatorImageButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorImageButton()
+}
